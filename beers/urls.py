06c59a1a991c40d51441d4165b9d60304eaa1604
Pablo Camino <pablo.camino@ambient-innovation.com>
2017-11-17 18:55:32 +0100

    Add first CBV

--- beers/urls.py
+++ beers/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 
-from beers.views import first_view, beer_list_view, beer_detail_view
+
+from beers.views import first_view, beer_detail_view, ListBeerView
 
 urlpatterns = [
     url(r'^$', first_view, name='first-view'),
-    url(r'^beer/list/$', beer_list_view, name='beer-list-view'),
+    url(r'^beer/list/$', ListBeerView.as_view(), name='beer-list-view'),
     url(r'^beer/detail/(?P<pk>\d+)/$', beer_detail_view, name='beer-detail-view'),
 ]
